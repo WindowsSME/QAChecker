5c6b5d764b2fdf110c2c028adf6df5b452012fc5
WindowsSME <139742802+WindowsSME@users.noreply.github.com>
2023-07-17 15:38:05 +0800

    Update QACompChecker.ps1

--- QACompChecker.ps1
+++ QACompChecker.ps1
@@ -1,3 +1,12 @@
+#Compliance Checker 5.0
+#CompChecker.ps1
+#Author: James Romeo Gaspar
+#OG: Version 1.0 | 5.26.2023
+#Revision: 2.0 | 5.28.2023 : Added Google Chrome fallback check; WorkspaceONE/Assist fallback check.
+#Revision: 3.0 | 6.6.2023 : Added GlobalProtect; Logfile set to hostname; Code optimization
+#Revision: 4.0 | 6.8.2023 : Added TPM check; Non-autorized accounts check; OS Version and Build check; Unused partition Check; Bios password status check, Wifi-Adapter status check, Installed OS Check, Added Device UDID check, Added Serial Number check
+#Revision: 5.0 | 7.17.2023 : Added Manufacturer check, re-arranged BIOS query timing
+
 $ErrorActionPreference = "SilentlyContinue"
 Import-Module -Name Microsoft.PowerShell.Utility
 
