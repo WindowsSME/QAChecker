0aff24061d7bb68b34af7eba9d6ca89094cf5a3d
{jaroga/} <139742802+WindowsSME@users.noreply.github.com>
2024-03-25 12:02:38 +0800

    Update QACompChecker.ps1

--- QACompChecker.ps1
+++ QACompChecker.ps1
@@ -1,4 +1,4 @@
-#Compliance Checker 5.1
+#Compliance Checker 6.0
 #CompChecker.ps1
 #Author: James Romeo Gaspar
 #OG: Version 1.0 | 5.26.2023
@@ -6,7 +6,7 @@
 #Revision: 3.0 | 6.6.2023 : Added GlobalProtect; Logfile set to hostname; Code optimization
 #Revision: 4.0 | 6.8.2023 : Added TPM check; Non-autorized accounts check; OS Version and Build check; Unused partition Check; Bios password status check, Wifi-Adapter status check, Installed OS Check, Added Device UDID check, Added Serial Number check
 #Revision: 5.0 | 7.17.2023 : Added Manufacturer check, re-arranged BIOS query timing
-#Revision: 5.1 | 3.25.2024 : Added additional fallback for Google Chrome check; added extraction of Computer OU
+#Revision: 6.0 | 3.25.2024 : Added additional fallback for Google Chrome check; added extraction of Computer OU
 
 $ErrorActionPreference = "SilentlyContinue"
 Import-Module -Name Microsoft.PowerShell.Utility
